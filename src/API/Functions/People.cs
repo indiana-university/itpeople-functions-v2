53ba755ecf7300a36850955ed49e40d51e20806a
John Hoerr <jhoerr@gmail.com>
2021-01-08 14:29:09 -0500

    Implement a Person pipeline

--- src/API/Functions/People.cs
+++ src/API/Functions/People.cs
@@ -1,27 +1,23 @@
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.Functions.Worker;
-using System.Net;
-using API.Data;
 using System.Threading.Tasks;
-using System.Text.Json;
+using CSharpFunctionalExtensions;
+
+using API.Data;
+using API.Middleware;
 
 namespace API.Functions
 {
     public static class People
     {        
         [FunctionName("GetByNetid")]
-        public static async Task<HttpResponseData> GetByNetid(
+        public static Task<HttpResponseData> GetByNetid(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "people/getbynetid")] HttpRequestData req) 
             {
                 var repo = new PeopleRepository();
-                var result = await repo.GetByNetId("");
-                return result.IsSuccess
-                    ? new HttpResponseData(HttpStatusCode.OK, JsonSerializer.Serialize(result.Value))
-                        {
-                            Headers = { { "Content-Type", "application/json; charset=utf-8" } }
-                        }
-                    : new HttpResponseData(HttpStatusCode.InternalServerError, result.Error);
+                return repo.GetByNetId("")
+                    .Finally(result => Response.Ok(req, result));
             }
     }
 }
