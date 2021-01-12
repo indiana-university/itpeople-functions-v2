6b4269bf952988c5b16d42b62b94303c97c74952
John Hoerr <jhoerr@gmail.com>
2021-01-12 16:01:23 -0500

    Move test harness files to own folder

---
+++ tests/API/Integration/Scaffolding/ApiTest.cs
@@ -0,0 +1,26 @@
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Integration
+{
+    public abstract class ApiTest
+    {
+        protected static HttpClient Http = new HttpClient(){
+            BaseAddress = new System.Uri("http://localhost:8080/api/")
+        };
+
+        protected static void AssertStatusCode(HttpResponseMessage resp, HttpStatusCode expected)
+        {
+            Assert.AreEqual(HttpStatusCode.OK, resp.StatusCode);
+        }
+
+        protected static async Task AssertStringContent(HttpResponseMessage resp, string expected)
+        {
+            var body = await resp.Content.ReadAsStringAsync();
+            Assert.AreEqual(expected, body);
+        }
+
+    }
+}
