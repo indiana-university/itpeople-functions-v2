d64906b0be10e87c25e3121dda0322d28775de65
John Hoerr <jhoerr@gmail.com>
2021-01-13 13:27:25 -0500

    Clean up Integration test harness

--- tests/API/Integration/Scaffolding/Harness.cs
+++ tests/API/Integration/Scaffolding/Harness.cs
@@ -8,14 +8,12 @@ namespace Integration
     [SetUpFixture]
     public class Harness
     {
-        private readonly bool _isCIBuild; 
         private readonly IDockerClient _client;
         private readonly SqlServerContainer _dbContainer;
         private readonly FunctionAppContainer _appContainer;
 
         public Harness()
         {
-            _isCIBuild = System.Environment.GetEnvironmentVariable("CI_BUILD") == "true";
             var uri  = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
                 ? new Uri("npipe://./pipe/docker_engine") 
                 : new Uri("unix:/var/run/docker.sock");
@@ -28,45 +26,24 @@ namespace Integration
         public void OneTimeSetup()
         {
             // Start SQL Server container
-            /*
-            if (_isCIBuild)
-            {
-                TestContext.Progress.WriteLine("🔍 CI_BUILD environment variable set to 'true'. Assuming that the database container was started by CI platform.");
-            }
-            else
-            {
-                TestContext.Progress.WriteLine("🔍 CI_BUILD environment variable is not set (or not set to 'true'). Will attempt to start the database container...");
-                _dbContainer.Start(_client).Wait(60 * 1000);
-            }
-            */
+            // _dbContainer.Start(_client).Wait(60 * 1000);
             // Wait for SQL Server container to finish starting
-            _dbContainer.Start(_client).Wait(60 * 1000);
-            _dbContainer.WaitUntilReady().Wait(60*1000);
-
-            _appContainer.BuildImage();
-            _appContainer.Start(_client).Wait(60*1000);
-            _appContainer.WaitUntilReady().Wait(60*1000);
+            // _dbContainer.WaitUntilReady().Wait(60*1000);
 
             // Build and start API Function app container
+            _appContainer.BuildImage();
+            _appContainer.Start(_client).Wait(60*1000);
             // Wait for API container to finish starting
+            _appContainer.WaitUntilReady().Wait(60*1000);
+
         }
 
         [OneTimeTearDown]
         public void OneTimeTeardown()
         {
             _appContainer.Stop(_client).Wait(60*1000);
-            _dbContainer.Stop(_client).Wait(60 * 1000);
-/*
-            if (_isCIBuild)
-            {
-                TestContext.Progress.WriteLine("🔍 CI_BUILD environment variable set to 'true'. Assuming that the database container will be stopped by CI platform.");
-            }
-            else
-            {
-                TestContext.Progress.WriteLine("🔍 CI_BUILD environment variable is not set (or not set to 'true'). Will attempt to stop the database container...");
-                _dbContainer.Stop(_client).Wait(60 * 1000);
-            }
-*/
+            _appContainer.Remove(_client).Wait(60*1000);
+            // _dbContainer.Stop(_client).Wait(60 * 1000);
         }
     }
 }
