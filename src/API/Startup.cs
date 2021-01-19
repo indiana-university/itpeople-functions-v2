b6cc7ce289043ba3c849dc0741c1d7bdabc0808d
John Hoerr <jhoerr@gmail.com>
2021-01-19 16:48:56 -0500

    Add some Postgres console logging.

--- src/API/Startup.cs
+++ src/API/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using System;
 using Database;
+using Npgsql.Logging;
 
 [assembly: FunctionsStartup(typeof(API.Startup))]
 namespace API
@@ -10,16 +11,20 @@ namespace API
     {
         public override void Configure(IFunctionsHostBuilder builder)
         {
+            NpgsqlLogManager.Provider = new ConsoleLoggingProvider(NpgsqlLogLevel.Info, true, true);
             MigrateDatabaseToLatest();
         }
 
         private static void MigrateDatabaseToLatest()
-        {
+        {            
             try
             {
+                Console.Error.WriteLine($"[Startup] Creating database context for migration...");
                 using (var context = PeopleContext.Create())
                 {
+                    Console.Error.WriteLine($"[Startup] Migrating database...");
                     context.Database.Migrate();
+                    Console.Error.WriteLine($"[Startup] Migrated database.");
                 }
             }
             catch (Exception e)
