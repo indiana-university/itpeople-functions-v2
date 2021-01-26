a2536fd7dae6be6e673716c21b68b248508da965
John Hoerr <jhoerr@gmail.com>
2021-01-26 14:42:26 -0500

    Create a generic API error info class

---
+++ src/Models/DTO/ApiError.cs
@@ -0,0 +1,11 @@
+using System.Net;
+using System.Collections.Generic;
+
+namespace Models
+{
+    public class ApiError
+    {
+        public int StatusCode { get; set; }
+        public List<string> Errors { get; set; }
+    }
+}
