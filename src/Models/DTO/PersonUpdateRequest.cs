378fc8d193f13ffbb41aa5631a42e3d663688b4f
John Hoerr <jhoerr@gmail.com>
2021-01-27 14:52:53 -0500

    Move and rename class

--- src/Models/DTO/PersonUpdateRequest.cs
+++ src/Models/DTO/PersonUpdateRequest.cs
@@ -1,16 +1,16 @@
 namespace Models
 {
-    public class PersonRequest : Entity
+    public class PersonUpdateRequest
     {
 
         /// The physical location (building, room) of this person.
-        public string Location { get; }    
+        public string Location { get; set; }    
         /// A collection of IT-related skills, expertise, or interests posessed by this person.
-        public string Expertise { get; }
+        public string Expertise { get; set; }
         /// A URL for a profile photo or headshot.
-        public string PhotoUrl { get;  }
+        public string PhotoUrl { get; set; }
         /// A collection of IT-related responsibilites of this person.
-        public Responsibilities Responsibilities { get; }
+        public Responsibilities Responsibilities { get; set; }
     }
     
 }
