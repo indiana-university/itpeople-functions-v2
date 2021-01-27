00a7a8a048aac39c5e3c3b11c33701c6cc8fd818
John Hoerr <jhoerr@gmail.com>
2021-01-27 14:52:26 -0500

    Add middleware to deserialize and validate request bodies

---
+++ src/API/Middleware/Request.cs
@@ -0,0 +1,45 @@
+using CSharpFunctionalExtensions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace API.Middleware
+{
+    public static class Request
+    {
+        public static Task<Result<T,Error>> DeserializeBody<T>(HttpRequest req)
+            => TryDeserializeBody<T>(req)
+                .Bind(body => ValidateBody<T>(body));
+
+        private static async Task<Result<T,Error>> TryDeserializeBody<T>(HttpRequest req)
+        {
+            try
+            {
+                var json = await req.ReadAsStringAsync();
+                var body = JsonConvert.DeserializeObject<T>(json);
+                return Pipeline.Success(body);
+            }
+            catch (Exception ex)
+            {
+                return Pipeline.BadRequest($"Failed to deserialize request body: {ex.Message}");
+            }
+        }
+
+        /// <summary>Deserialize the request body to an instance of specified type and validate all properties. If valid, the instance is returned.</summary>
+        private static Result<T, Error> ValidateBody<T>(T body)
+        {
+            var validationContext = new ValidationContext(body, null, null);
+            var results = new List<ValidationResult>();
+            Validator.TryValidateObject(body, validationContext, results, true);
+
+            return results.Count > 0
+            ? Pipeline.BadRequest(results.Select(r => r.ErrorMessage))
+            : Pipeline.Success(body);
+        }
+    }
+}
