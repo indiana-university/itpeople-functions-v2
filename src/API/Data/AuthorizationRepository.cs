64ed72edbb8f1935c4e1d365e11bf5b61795c96b
John Hoerr <jhoerr@gmail.com>
2021-01-28 11:10:04 -0500

    Refactor AuthorizationRepository

--- src/API/Data/AuthorizationRepository.cs
+++ src/API/Data/AuthorizationRepository.cs
@@ -9,7 +9,7 @@ using Models;
 
 namespace API.Data
 {
-    public class AuthorizationRepository
+    public class AuthorizationRepository : DataRepository
     {
         public static Result<bool, Error> AuthorizeModification(EntityPermissions permissions) 
             => permissions.HasFlag(EntityPermissions.Put)
@@ -17,69 +17,62 @@ namespace API.Data
                 : Pipeline.Unauthorized();
 
 
-        internal async static Task<Result<EntityPermissions, Error>> DeterminePersonPermissions(HttpRequest req, string requestorNetid, int personId)
+        internal static Task<Result<EntityPermissions, Error>> DeterminePersonPermissions(HttpRequest req, string requestorNetid, int personId)
+            => ExecuteDbPipeline("resolve person permissions", db =>
+                FetchPeople(db, requestorNetid, personId)
+                .Bind(tup => ResolvePersonPermissions(tup.requestor, tup.target))
+                .Tap(perms => AddResponseHeaders(req, perms)));
+        
+        private static async Task<Result<(Person requestor, Person target),Error>> FetchPeople(PeopleContext db, string requestorNetid, int personId)
         {
-            // if requestor is a service admin, they can get/put
-            // if requestor manages a person's unit, they can get/put
-            // if requestor is person, they can get/put
-            // otherwise, get only
+            var persons = await db.People
+                .Include(p => p.UnitMemberships)
+                .Where(p => p.Netid.ToLower() == requestorNetid.ToLower() || p.Id == personId)
+                .ToListAsync();
+            var target = persons.FirstOrDefault(p => p.Id == personId);
+            var requestor = persons.FirstOrDefault(p => p.Netid.ToLower() == requestorNetid.ToLower());
+            return target == null
+                ? Pipeline.NotFound("No person was found with the ID provided.")
+                : Pipeline.Success((requestor, target));
+        }
 
-            try
+        public static Result<EntityPermissions,Error> ResolvePersonPermissions(Person requestor, Person target)
+        {
+            //By default a user can only "get"
+            var result = EntityPermissions.Get;
+
+            if (requestor != null)
             {
-                using (var db = PeopleContext.Create())
+                var requestorManagedUnits = requestor
+                    .UnitMemberships
+                    .Where(m => m.Permissions == UnitPermissions.Owner || m.Permissions == UnitPermissions.ManageMembers)
+                    .Select(m => m.UnitId)
+                    .ToList();
+
+                var personMemberOfUnits = target
+                    .UnitMemberships
+                    .Select(m => m.UnitId)
+                    .ToList();
+                
+                var matches = requestorManagedUnits.Intersect(personMemberOfUnits);
+
+                if (requestor.IsServiceAdmin // requestor is a service admin
+                    || requestor.Id == target.Id // requestor and and target person are the same
+                    || matches.Count() > 0)  // requestor manages a unit that the target person is a member of
                 {
-                    //By default a user can only "get"
-                    var result = EntityPermissions.Get;
-                    
-                    // Fetch the person entity for the requestor and target person.
-                    var persons = await db.People
-                        .Include(p => p.UnitMemberships)
-                        .Where(p => p.Netid.ToLower() == requestorNetid.ToLower() || p.Id == personId)
-                        .ToListAsync();
-
-                    var requestor = persons.FirstOrDefault(p => p.Netid.ToLower() == requestorNetid.ToLower());
-                    var target = persons.FirstOrDefault(p => p.Id == personId);
-
-                    // If we didn't find the target person return a 404
-                    if(target == null)
-                    {
-                        return Pipeline.NotFound("No person was found with the ID provided.");
-                    }
-
-                    if (requestor != null)
-                    {
-                        var requestorManagedUnits = requestor
-                            .UnitMemberships
-                            .Where(m => m.Permissions == UnitPermissions.Owner || m.Permissions == UnitPermissions.ManageMembers)
-                            .Select(m => m.UnitId)
-                            .ToList();
-
-                        var personMemberOfUnits = target
-                            .UnitMemberships
-                            .Select(m => m.UnitId)
-                            .ToList();
-                        
-                        var matches = requestorManagedUnits.Intersect(personMemberOfUnits);
-
-                        if (requestor.IsServiceAdmin // requestor is a service admin
-                            || requestor.Id == target.Id // requestor and and target person are the same
-                            || matches.Count() > 0)  // requestor manages a unit that the target person is a member of
-                        {
-                            result = EntityPermissions.Get | EntityPermissions.Put;
-                        }   
-                    }
-
-                    // attach those permission to the response headers.
-                    req.HttpContext.Response.Headers[Response.Headers.XUserPermissions] = result.ToString();
-                    
-                    // return the entity permissions to the next step of the pipeline.
-                    return Pipeline.Success(result);
-                }
+                    result = EntityPermissions.Get | EntityPermissions.Put;
+                }   
             }
-            catch (System.Exception ex)
-            {
-                return Pipeline.InternalServerError(ex.Message);
-            }
+
+            // return the entity permissions to the next step of the pipeline.
+            return Pipeline.Success(result);
+        }
+
+        private static void AddResponseHeaders(HttpRequest req, EntityPermissions permissions)
+        {
+            req.HttpContext.Response.Headers[Response.Headers.XUserPermissions] = permissions.ToString();
         }
     }
+
+
 }
