14ce8a3d9a3d573f00a82f8cf862a54ca2515cbf
jardrake <jardrake@iu.edu>
2021-02-17 10:43:57 -0500

    wrap Unit Member Integration tests based on the function

--- tests/API/Integration/UnitMembersTests.cs
+++ tests/API/Integration/UnitMembersTests.cs
@@ -12,62 +12,68 @@ namespace Integration
     [Category("UnitMembers")]
     public class UnitMembersTests : ApiTest
     {
-        [Test]
-        public async Task FetchesAllUnitMembers()
-        {
-            var resp = await GetAuthenticated("memberships");
-            AssertStatusCode(resp, HttpStatusCode.OK);
-            var actual = await resp.Content.ReadAsAsync<List<UnitMemberResponse>>();
-            var expectedIds = new []{
-                TestEntities.UnitMembers.RSwansonLeaderId,
-                TestEntities.UnitMembers.LkNopeSubleadId,
-                TestEntities.UnitMembers.BWyattMemberId,
-            };
-            AssertIdsMatchContent(expectedIds, actual);
+        public class GetAll : ApiTest
+		{
+            [Test]
+            public async Task FetchesAllUnitMembers()
+            {
+                var resp = await GetAuthenticated("memberships");
+                AssertStatusCode(resp, HttpStatusCode.OK);
+                var actual = await resp.Content.ReadAsAsync<List<UnitMemberResponse>>();
+                var expectedIds = new []{
+                    TestEntities.UnitMembers.RSwansonLeaderId,
+                    TestEntities.UnitMembers.LkNopeSubleadId,
+                    TestEntities.UnitMembers.BWyattMemberId,
+                };
+                AssertIdsMatchContent(expectedIds, actual);
+            }
+
+            [Test]
+            public async Task FetchAllHasExpectedRelations()
+            {
+                var resp = await GetAuthenticated("memberships");
+                AssertStatusCode(resp, HttpStatusCode.OK);
+                var actual = await resp.Content.ReadAsAsync<List<UnitMemberResponse>>();
+                var ron = actual.SingleOrDefault(a => a.Id == TestEntities.UnitMembers.RSwansonLeaderId);
+                Assert.NotNull(ron.Person);
+                Assert.NotNull(ron.Unit);
+                Assert.NotNull(ron.Unit.Parent);
+                Assert.NotNull(ron.MemberTools);
+            }
         }
 
-        [Test]
-        public async Task FetchAllHasExpectedRelations()
-        {
-            var resp = await GetAuthenticated("memberships");
-            AssertStatusCode(resp, HttpStatusCode.OK);
-            var actual = await resp.Content.ReadAsAsync<List<UnitMemberResponse>>();
-            var ron = actual.SingleOrDefault(a => a.Id == TestEntities.UnitMembers.RSwansonLeaderId);
-            Assert.NotNull(ron.Person);
-            Assert.NotNull(ron.Unit);
-            Assert.NotNull(ron.Unit.Parent);
-            Assert.NotNull(ron.MemberTools);
-        }
+        public class GetOne : ApiTest
+		{
+            [TestCase(TestEntities.UnitMembers.RSwansonLeaderId, HttpStatusCode.OK)]
+            [TestCase(9999, HttpStatusCode.NotFound)]
+            public async Task HasCorrectStatusCode(int id, HttpStatusCode expectedStatus)
+            {
+                var resp = await GetAuthenticated($"memberships/{id}");
+                AssertStatusCode(resp, expectedStatus);
+            }
 
-        [TestCase(TestEntities.UnitMembers.RSwansonLeaderId, HttpStatusCode.OK)]
-        [TestCase(9999, HttpStatusCode.NotFound)]
-        public async Task HasCorrectStatusCode(int id, HttpStatusCode expectedStatus)
-        {
-            var resp = await GetAuthenticated($"memberships/{id}");
-            AssertStatusCode(resp, expectedStatus);
-        }
-
-        [Test]
-        public async Task ResponseHasCorrectShape()
-        {
-            var resp = await GetAuthenticated($"memberships/{TestEntities.UnitMembers.RSwansonLeaderId}");
-            AssertStatusCode(resp, HttpStatusCode.OK);
-            var actual = await resp.Content.ReadAsAsync<UnitMemberResponse>();
-            var expected = TestEntities.UnitMembers.RSwansonDirector;
-            Assert.AreEqual(expected.Id, actual.Id);
-            Assert.AreEqual(expected.UnitId, actual.UnitId);
-            Assert.AreEqual(expected.Role, actual.Role);
-            Assert.AreEqual(expected.Permissions, actual.Permissions);
-            Assert.AreEqual(expected.PersonId, actual.PersonId);
-            Assert.AreEqual(expected.Title, actual.Title);
-            Assert.AreEqual(expected.Percentage, actual.Percentage);
-            Assert.AreEqual(expected.Notes, actual.Notes);
-            // relations
-            Assert.NotNull(actual.Person);
-            Assert.AreEqual(expected.Person.Id, actual.Person.Id);
-            Assert.NotNull(actual.Unit);
-            Assert.AreEqual(expected.Unit.Id, actual.Unit.Id);
-            Assert.NotNull(actual.MemberTools);
+            [Test]
+            public async Task ResponseHasCorrectShape()
+            {
+                var resp = await GetAuthenticated($"memberships/{TestEntities.UnitMembers.RSwansonLeaderId}");
+                AssertStatusCode(resp, HttpStatusCode.OK);
+                var actual = await resp.Content.ReadAsAsync<UnitMemberResponse>();
+                var expected = TestEntities.UnitMembers.RSwansonDirector;
+                Assert.AreEqual(expected.Id, actual.Id);
+                Assert.AreEqual(expected.UnitId, actual.UnitId);
+                Assert.AreEqual(expected.Role, actual.Role);
+                Assert.AreEqual(expected.Permissions, actual.Permissions);
+                Assert.AreEqual(expected.PersonId, actual.PersonId);
+                Assert.AreEqual(expected.Title, actual.Title);
+                Assert.AreEqual(expected.Percentage, actual.Percentage);
+                Assert.AreEqual(expected.Notes, actual.Notes);
+                // relations
+                Assert.NotNull(actual.Person);
+                Assert.AreEqual(expected.Person.Id, actual.Person.Id);
+                Assert.NotNull(actual.Unit);
+                Assert.AreEqual(expected.Unit.Id, actual.Unit.Id);
+                Assert.NotNull(actual.MemberTools);
+            }
         }
     }
 }
