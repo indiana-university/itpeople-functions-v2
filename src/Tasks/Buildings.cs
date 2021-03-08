774937030955d8f02bb4aa00a0ef430675121daa
John Hoerr <jhoerr@gmail.com>
2021-03-08 10:16:14 -0500

    Move Task DTOs next to functions

--- src/Tasks/Buildings.cs
+++ src/Tasks/Buildings.cs
@@ -11,6 +11,7 @@ using Models;
 using Database;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
+using Newtonsoft.Json;
 
 namespace Tasks
 {
@@ -90,5 +91,23 @@ namespace Tasks
             req.Headers.Authorization = new AuthenticationHeaderValue("Basic", basicAuth);
             return req;
         }
+    }
+
+    public class DenodoResponse<T>
+    {
+        [JsonProperty("name")] public string Name { get; set; }
+        [JsonProperty("elements")] public IEnumerable<T> Elements { get; set; }
+    }
+
+    public class DenodoBuilding
+    {
+        [JsonProperty("building_name")] public string Name { get; set; }
+        [JsonProperty("building_code")] public string BuildingCode { get; set; }
+        [JsonProperty("site_code")] public string SiteCode { get; set; }
+        [JsonProperty("building_long_description")] public string Description { get; set; }
+        [JsonProperty("street")] public string Street { get; set; }
+        [JsonProperty("city")] public string City { get; set; }
+        [JsonProperty("state")] public string State { get; set; }
+        [JsonProperty("zip")] public string Zip { get; set; }
     }
 }
