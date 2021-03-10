f1aad3cd8249a4bf603fca7f3390520adeeccc2a
John Hoerr <jhoerr@gmail.com>
2021-03-10 12:14:23 -0500

    Add some DB and HTTP response deserialization helpers/loggers

--- src/Tasks/Utils.cs
+++ src/Tasks/Utils.cs
@@ -1,4 +1,8 @@
 using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Database;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 
 namespace Tasks
 {
@@ -12,6 +16,52 @@ namespace Tasks
                 throw new Exception($"Missing required environment setting: {key}");
             }
             return value;
+        }
+
+        public static async Task<T> DeserializeResponse<T>(IDurableActivityContext ctx, HttpResponseMessage resp, string description)
+        {
+            if (false == resp.IsSuccessStatusCode)
+            {
+                var msg = await resp.Content.ReadAsStringAsync();
+                var err = new {url=resp.RequestMessage.RequestUri, status=resp.StatusCode, msg=msg??("none")};
+                Logging.GetLogger(ctx, err).Error($"Failed HTTP operation to {description}");
+                throw new Exception($"Failed HTTP operation to {description}. URL: {err.url} Status: {err.status}. Message: {err.msg}");
+            }
+            return await resp.Content.ReadAsAsync<T>();
+        }
+
+        public static async Task DatabaseCommand(IDurableActivityContext ctx, string description, Func<PeopleContext, Task> op)
+        {
+            try
+            {
+                var connStr = Utils.Env("DatabaseConnectionString", required: true);
+                using (var db = PeopleContext.Create(connStr))
+                {
+                    await op(db);
+                }
+            }
+            catch (Exception ex)
+            {
+                Logging.GetLogger(ctx).Error(ex, $"Failed to execute database command: {description}.");
+                throw;
+            }
+        }
+
+        public static async Task<T> DatabaseQuery<T>(IDurableActivityContext ctx, string description, Func<PeopleContext, Task<T>> op)
+        {
+            try
+            {
+                var connStr = Utils.Env("DatabaseConnectionString", required: true);
+                using (var db = PeopleContext.Create(connStr))
+                {
+                    return await op(db);
+                }
+            }
+            catch (Exception ex)
+            {
+                Logging.GetLogger(ctx).Error(ex, $"Failed to execute database query: {description}.");
+                throw;
+            }
         }
     }
 }
