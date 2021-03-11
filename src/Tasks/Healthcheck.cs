43f0906151e57859903b2b8cba9634423b6a914f
John Hoerr <jhoerr@gmail.com>
2021-03-11 13:52:40 -0500

    Debugging: add HTTP endpoint that will resolve on-prem FQDNs to IPs
    Can we even do this?

--- src/Tasks/Healthcheck.cs
+++ src/Tasks/Healthcheck.cs
@@ -12,4 +12,31 @@ namespace Tasks
         public static string Ping(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "ping")] HttpRequest req) 
                 => "Pong!";
+
+        [FunctionName(nameof(DnsCheck))]
+        public static string DnsCheck(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "dnsCheck")] HttpRequest req)
+        {
+            var hosts = new[]{
+                "ebidvt.uits.iu.edu",
+                "apps.iu.edu",
+                "prs.apps.iu.edu",
+                "esdbp57p.uits.iu.edu"
+            };
+            return string.Join("; ", hosts.Select(TryGetIP));
+        }
+
+        private static string TryGetIP(string hostName)
+        {
+            try
+            {
+                var ip = System.Net.Dns.GetHostEntry(hostName);
+                return $"{hostName}:{string.Join(",", ip.AddressList.Select(ipx=>ipx.ToString()))}";
+            }
+            catch (Exception ex)
+            {
+                return $"{hostName}:{ex.Message}";
+            }
+        }
+    }
 }
