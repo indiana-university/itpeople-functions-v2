144235a282660ddfe6f9953e74d3df15e76c8176
Venkata Bhupathi <venkatanbhupathi@gmail.com>
2021-10-18 13:39:07 -0400

    Add data annotation for enums

--- src/Models/Enums/Area.cs
+++ src/Models/Enums/Area.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 
@@ -8,8 +9,10 @@ namespace Models
     {
         
         /// University Information Technology Services (UITS) IT units are part of IU's central tech and support organization.
+        [Display(Name="UITS Units")]
         Uits=1,
         /// Edge IT units are integrated directly into colleges and academic departments.
+        [Display(Name="Edge Units")]
         Edge=2
     }
 
