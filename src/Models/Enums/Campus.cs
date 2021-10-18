683ecb035bd4db9ba1c1c219934b03c8c6b3c54b
Venkata Bhupathi <venkatanbhupathi@gmail.com>
2021-10-18 19:22:24 -0400

    Add Campus with data annotations

---
+++ src/Models/Enums/Campus.cs
@@ -0,0 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Models
+{
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum Campus
+    {
+        
+       
+        [Display(Name="Bloomington")]
+        BL=1,
+        [Display(Name="IUPUI (Indianapolis)")]
+        IN=2, 
+        [Display(Name="IUPUC (Columbus)")]
+        CO=3,
+        [Display(Name="East (Richmond)")]
+        EA=4,
+        [Display(Name="Fort Wayne")]
+        FW=5,
+        [Display(Name="Kokomo")]
+        KO=6,
+        [Display(Name="Northwest (Gary)")]
+        NW=7,
+        [Display(Name="South Bend")]
+        SB=8,
+        [Display(Name="Southeast (New Albany)")]
+        SE=9
+    }
+
+}
