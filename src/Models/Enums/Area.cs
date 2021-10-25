1be1554b59accc7e9de22fd0d75744cdb4bc15e3
Venkata Bhupathi <venkatanbhupathi@gmail.com>
2021-10-25 15:04:37 -0400

    Make Flags for Area to make it work in People page

--- src/Models/Enums/Area.cs
+++ src/Models/Enums/Area.cs
@@ -1,19 +1,28 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 
 namespace Models
 {
-    [JsonConverter(typeof(StringEnumConverter))]
+     [Flags]    
     public enum Area
-    {
+    { 
         
         /// University Information Technology Services (UITS) IT units are part of IU's central tech and support organization.
         [Display(Name="UITS Units")]
-        Uits=1,
+        Uits    =   0b00000000000000001,
         /// Edge IT units are integrated directly into colleges and academic departments.
         [Display(Name="Edge Units")]
-        Edge=2
+        Edge    =   0b00000000000000010
+
+    }
+
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum AreaPropDoc
+    {
+        Uits    =   Area.Uits,
+        Edge    =   Area.Edge
     }
 
 }
