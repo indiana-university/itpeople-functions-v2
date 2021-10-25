eb9ef5e2592e45c1e3ec7f32e88520e3e554ad0e
Venkata Bhupathi <venkatanbhupathi@gmail.com>
2021-10-25 15:04:27 -0400

    Make Flags for Campus to make it work in People page

--- src/Models/Enums/Campus.cs
+++ src/Models/Enums/Campus.cs
@@ -1,32 +1,45 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 
 namespace Models
 {
-    [JsonConverter(typeof(StringEnumConverter))]
+    [Flags]    
     public enum Campus
-    {
-        
-       
+    { 
         [Display(Name="Bloomington")]
-        BL=1,
+        BL  =   0b00000000000000001,
         [Display(Name="IUPUI (Indianapolis)")]
-        IN=2, 
+        IN  =   0b00000000000000010, 
         [Display(Name="IUPUC (Columbus)")]
-        CO=3,
+        CO  =   0b00000000000000100,
         [Display(Name="East (Richmond)")]
-        EA=4,
+        EA  =   0b00000000000001000,
         [Display(Name="Fort Wayne")]
-        FW=5,
+        FW  =   0b00000000000010000,
         [Display(Name="Kokomo")]
-        KO=6,
+        KO  =   0b00000000000100000,
         [Display(Name="Northwest (Gary)")]
-        NW=7,
+        NW  =   0b00000000001000000,
         [Display(Name="South Bend")]
-        SB=8,
+        SB  =   0b00000000010000000,
         [Display(Name="Southeast (New Albany)")]
-        SE=9
+        SE  =   0b00000000100000000
+    }
+
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum CampusPropDoc
+    {
+        BL  =   Campus.BL,
+        IN  =   Campus.IN, 
+        CO  =   Campus.CO,
+        EA  =   Campus.EA,
+        FW  =   Campus.FW,
+        KO  =   Campus.KO,
+        NW  =   Campus.NW,
+        SB  =   Campus.SB,
+        SE  =   Campus.SE        
     }
 
 }
