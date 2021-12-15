0ea9a3e16dc5f422218f30509eb0ce2eec98eb70
Jason Francis <jkfranci@iu.edu>
2021-12-15 12:07:27 -0500

    WIP: Adding Expires property to AuthenticatedUser This will let us make sure the token is still valid before sending a request.

--- src/Web/Models/AuthenticatedUser.cs
+++ src/Web/Models/AuthenticatedUser.cs
@@ -1,8 +1,20 @@
+using System;
+
 namespace web
 {
 	public class AuthenticatedUser
 	{
 		public string AccessToken {get;set;}
 		public string Username {get;set;}
+		public DateTimeOffset Expires {get; set;}
+
+		public AuthenticatedUser() {}
+		public AuthenticatedUser(string token, string username, string exp)
+		{
+			AccessToken = token;
+			Username = username;
+			var expAsDouble = double.Parse(exp);
+			Expires = new DateTimeOffset(1970, 1, 1, 0, 0, 0, 0, new TimeSpan(0, 0, 0)).AddSeconds(expAsDouble);
+		}
 	}
 }
