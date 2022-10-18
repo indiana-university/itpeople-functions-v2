c0dab7785451c8497a8562aa71ea78bff204e9e5
Jason Francis <jkfranci@iu.edu>
2022-10-18 17:28:00 -0400

    Adding the migration for Department.ReportSupportingUnit. In spite of all the tweaks to the models this came out clean as a whistle.  Good job EF.

---
+++ src/Database/Migrations/20221018211939_AddDepartmentReportSupportingUnit.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Database.Migrations
+{
+    public partial class AddDepartmentReportSupportingUnit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "report_supporting_unit_id",
+                schema: "public",
+                table: "departments",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_departments_report_supporting_unit_id",
+                schema: "public",
+                table: "departments",
+                column: "report_supporting_unit_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_departments_units_report_supporting_unit_id",
+                schema: "public",
+                table: "departments",
+                column: "report_supporting_unit_id",
+                principalSchema: "public",
+                principalTable: "units",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_departments_units_report_supporting_unit_id",
+                schema: "public",
+                table: "departments");
+
+            migrationBuilder.DropIndex(
+                name: "ix_departments_report_supporting_unit_id",
+                schema: "public",
+                table: "departments");
+
+            migrationBuilder.DropColumn(
+                name: "report_supporting_unit_id",
+                schema: "public",
+                table: "departments");
+        }
+    }
+}
