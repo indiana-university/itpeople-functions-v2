1e9338b44b27d56d05b1804fe636d38cd3ebcbc9
Jason Francis <jkfranci@gmail.com>
2022-10-24 15:28:21 -0400

    Generating OpenAPI documentation.

--- src/API/Functions/Notifications.cs
+++ src/API/Functions/Notifications.cs
@@ -6,12 +6,23 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using CSharpFunctionalExtensions;
 using API.Data;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using System.Net;
+using System.Net.Mime;
+using Models;
+using System.Collections.Generic;
+using Microsoft.OpenApi.Models;
 
 namespace API.Functions
 {
 	public static class Notifications
 	{
 		[FunctionName(nameof(Notifications.NotificationsGetAll))]
+		[OpenApiOperation(nameof(Notifications.NotificationsGetAll), nameof(Notifications), Summary="List notifications", Description = @"List all Notifications for system admins." )]
+		[OpenApiResponseWithBody(HttpStatusCode.OK, MediaTypeNames.Application.Json, typeof(List<Notification>))]
+		[OpenApiResponseWithBody(HttpStatusCode.BadRequest, MediaTypeNames.Application.Json, typeof(ApiError), Description="The search query was malformed or incorrect. See response content for additional information.")]
+		[OpenApiResponseWithBody(HttpStatusCode.Forbidden, MediaTypeNames.Application.Json, typeof(ApiError), Description = "You do not have permission to list Notifications.")]
+		[OpenApiParameter("ShowReviewed=1", In=ParameterLocation.Query, Description="Include Notifications that have already been reviewed in the results.")]
 		public static Task<IActionResult> NotificationsGetAll(
 			[HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "notifications")] HttpRequest req)
 			=> Security.Authenticate(req)
