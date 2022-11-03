e3f4c571b07ff7a91c22f025d247bd5a0e3ee790
Jason Francis <jkfranci@gmail.com>
2022-11-03 14:20:50 -0400

    Moving EnrichedSRR class. Unit.razor is already way too huge.

--- src/Models/DTO/SupportRelationshipRequest.cs
+++ src/Models/DTO/SupportRelationshipRequest.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Models
 {
     public class SupportRelationshipRequest
@@ -11,4 +13,51 @@ namespace Models
         /// The ID of the support type in this relationship
         public int? SupportTypeId { get; set; }
     }
+
+    public class EnrichedSRR : SupportRelationshipRequest
+	{
+		private Models.UnitResponse _Unit;
+		public Models.UnitResponse Unit
+		{
+			get => _Unit;
+			set
+			{
+				_Unit = value;
+				UnitId = _Unit.Id;
+			}
+		}
+
+		private Department _Department;
+		public Department Department
+		{
+			get => _Department;
+			set
+			{
+				_Department = value;
+				DepartmentId = _Department?.Id ?? 0;
+			}
+		}
+
+		private SupportType _SupportType;
+		public SupportType SupportType
+		{
+			get => _SupportType;
+			set
+			{
+				_SupportType = value;
+				SupportTypeId = _SupportType?.Id;
+			}
+		}
+
+		private UnitResponse? _ReportSupportingUnit;
+		public UnitResponse? ReportSupportingUnit
+		{
+			get => _ReportSupportingUnit;
+			set
+			{
+				_ReportSupportingUnit = value;
+				ReportSupportingUnitId = _ReportSupportingUnit?.Id ?? 0;
+			}
+		}
+	}
 }
