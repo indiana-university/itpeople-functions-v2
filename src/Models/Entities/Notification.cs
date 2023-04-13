8cfd19d0e1e9f3c564c50b3c6ccb701e974efd6d
Jason Francis <jkfranci@iu.edu>
2023-04-13 14:45:47 -0400

    New Postgres package insists on UTC DateTimes. This gets a few of the broken tests passing.

--- src/Models/Entities/Notification.cs
+++ src/Models/Entities/Notification.cs
@@ -7,7 +7,7 @@ namespace Models
 	public class Notification : Entity
 	{
 		[Required]
-		public DateTime Created { get; set; } = DateTime.Now;
+		public DateTime Created { get; set; } = DateTime.UtcNow;
 		[Required(AllowEmptyStrings = false)]
 		public string Message { get; set; }
 		public DateTime? Reviewed { get; set; }
