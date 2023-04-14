7050ca68ed3b17d4da0464471870c63ace9252ff
Jason Francis <jkfranci@gmail.com>
2023-04-14 11:09:37 -0400

    State State Server Fix (#95)
    * Fixing the DockerFile and csproj file for the "stat server" used to reset the state of the database between tests.
    
    * Fixing test, new packages seem to change the order of the IDs in this error.
    
    ---------
    
    Co-authored-by: Jason Francis <jkfranci@iu.edu>

--- tests/API/Integration/UnitsTests.cs
+++ tests/API/Integration/UnitsTests.cs
@@ -396,7 +396,7 @@ namespace Integration
                 var actual = await resp.Content.ReadAsAsync<ApiError>();
 
                 Assert.AreEqual(1, actual.Errors.Count);
-                Assert.Contains("Unit 1 has child units, with ids: 2, 4, 3. These must be reassigned prior to deletion.", actual.Errors);
+                Assert.Contains("Unit 1 has child units, with ids: 2, 3, 4. These must be reassigned prior to deletion.", actual.Errors);
                 Assert.AreEqual("(none)", actual.Details);
             }
 
