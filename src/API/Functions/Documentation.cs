a01004f29f8a447717b77c770050f57c19f910e0
Jason Francis <jkfranci@gmail.com>
2023-10-18 13:45:19 -0400

    Addressing HTML Language attribute.

--- src/API/Functions/Documentation.cs
+++ src/API/Functions/Documentation.cs
@@ -20,13 +20,18 @@ namespace API.Functions
 
         public static string ApiUIMarkup = @"
 <!DOCTYPE html>
-<html>
+<html lang=""en"">
 <head>
     <title>IT People API</title>
     <meta charset=""utf-8"" />
     <meta name=""viewport"" content=""width=device-width, initial-scale=1"">
     <link href=""https://fonts.googleapis.com/css?family=Montserrat:300,400,700|Roboto:300,400,700"" rel=""stylesheet"">
-    <style> body { margin: 0; padding: 0; }</style>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+        }
+    </style>
 </head>
 <body>
     <redoc spec-url='openapi/v3.json'></redoc>
