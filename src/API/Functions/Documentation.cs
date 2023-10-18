697cf5f0e8dfd9d5749a9e3c58fd7a30f197aecf
Jason Francis <jkfranci@gmail.com>
2023-10-18 15:13:28 -0400

    Ensure the first heading tag it sees is an H1.

--- src/API/Functions/Documentation.cs
+++ src/API/Functions/Documentation.cs
@@ -53,7 +53,7 @@ namespace API.Functions
     <redoc spec-url='openapi/v3.json'></redoc>
     <script src=""https://cdn.jsdelivr.net/npm/redoc@next/bundles/redoc.standalone.js""> </script>
 
-    <div style=""text-align: center""><h6>Indiana University</h6><p style=""line-height: 1.5"">Service Management Technologies</p></div>
+    <div style=""text-align: center""><h1 style=""font-size: 1em"">Indiana University</h1><p style=""line-height: 1.5"">Service Management Technologies</p></div>
 </body>
 </html>
 ";
