872e51311541658fcf8fb2a804433a34de19e306
Jason Francis <jkfranci@gmail.com>
2023-10-19 13:02:50 -0400

    Starting to use SiteHost's fork of Swagger-UI There are still things to fix.

--- src/API/Functions/Documentation.cs
+++ src/API/Functions/Documentation.cs
@@ -18,6 +18,7 @@ namespace API.Functions
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "/")] HttpRequest req)
                 => new ContentResult(){ StatusCode=200, Content = ApiUIMarkup, ContentType="text/html" };
 
+        /*
         public static string ApiUIMarkup = @"
 <!DOCTYPE html>
 <html lang=""en"">
@@ -57,5 +58,102 @@ namespace API.Functions
 </body>
 </html>
 ";
+        */
+        public static string ApiUIMarkup = @"
+<!-- HTML for static distribution bundle build -->
+<!DOCTYPE html>
+<html lang=""en"">
+<head>
+	<meta charset=""UTF-8"">
+	<title>Siteimprove API</title> <!-- SITEIMPROVE EDIT -->
+	<link rel=""stylesheet"" type=""text/css"" href=""https://itpeople-test.iu.edu/swagger-ui/swagger-ui.css"" />
+	<link rel=""stylesheet"" type=""text/css"" href=""https://itpeople-test.iu.edu/swagger-ui/index.css"" />
+	<link rel=""icon"" type=""image/png"" href=""https://itpeople-test.iu.edu/swagger-ui/favicon-32x32.png"" sizes=""32x32"" />
+	<link rel=""icon"" type=""image/png"" href=""https://itpeople-test.iu.edu/swagger-ui/favicon-16x16.png"" sizes=""16x16"" />
+	<!-- SITEIMPROVE EDIT -->
+	<!-- <link href=""siteimprove.css"" type=""text/css"" rel=""stylesheet"" /> -->
+	<!-- <link href=""siteimprove-swagger-theme.css"" type=""text/css"" rel=""stylesheet"" /> -->
+	<style>
+		html {
+			box-sizing: border-box;
+			overflow: -moz-scrollbars-vertical;
+			overflow-y: scroll;
+		}
+
+		*,
+		*:before,
+		*:after {
+			box-sizing: inherit;
+		}
+
+		body {
+			margin: 0;
+			background: #fafafa;
+		}
+
+		.top-fixed {
+			position: fixed;
+			top: 0;
+			left: 0;
+			background-color: #eee;
+			padding: 3px 10px;
+		}
+	</style>
+</head>
+
+<body>
+	<div id=""swagger-ui""></div>
+
+	<script src=""https://itpeople-test.iu.edu/swagger-ui/swagger-ui-bundle.js"" charset=""UTF-8""></script>
+	<script src=""https://itpeople-test.iu.edu/swagger-ui/swagger-ui-standalone-preset.js"" charset=""UTF-8""></script>
+	<script src=""https://itpeople-test.iu.edu/swagger-ui/swagger-initializer.js"" charset=""UTF-8""> </script>
+	<!-- SITEIMPROVE EDIT -->
+	<!-- <script src=""https://itpeople-test.iu.edu/swagger-ui/jquery-3.6.0.min.js"" type=""text/javascript""></script> -->
+	<!-- <script src=""https://itpeople-test.iu.edu/swagger-ui/siteimprove.js"" type=""text/javascript""></script> -->
+
+	<script>
+		window.onload = function () {
+			var url = ""/openapi/v3.json"";
+
+			// Pre load translate...
+			if (window.SwaggerTranslator) {
+				window.SwaggerTranslator.translate();
+			}
+
+			// Begin Swagger UI call region
+			const ui = SwaggerUIBundle({
+				url: url,
+				dom_id: '#swagger-ui',
+				deepLinking: true,
+				presets: [
+					SwaggerUIBundle.presets.apis,
+					SwaggerUIStandalonePreset
+				],
+				plugins: [
+					SwaggerUIBundle.plugins.DownloadUrl
+				],
+				layout: ""StandaloneLayout"",
+				onComplete: function (swaggerApi, swaggerUi) {
+					if (window.SwaggerTranslator) {
+						window.SwaggerTranslator.translate();
+					}
+				},
+				onFailure: function (data) {
+					log(""Unable to Load SwaggerUI"");
+				},
+				docExpansion: ""none"",
+				jsonEditor: false,
+				// defaultModelRendering: window.siteimprove.optionDefaultModelRendering(""False"" === ""True""), // SITEIMPROVE EDIT
+				showRequestHeaders: false,
+				// requestInterceptor: function (req) { req.url = req.url.replace(""https://localhost/"", ""https://localhost:443/""); return req; }
+			});
+			// End Swagger UI call region
+
+			window.ui = ui;
+		};
+	</script>
+</body>
+</html>
+        ";
     }
 }
