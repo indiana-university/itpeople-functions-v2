8d96901b25609523ec5f58f143e85b8b78fa3e4a
Jason Francis <jkfranci@gmail.com>
2025-04-15 14:41:00 -0400

    More library updates RivetBlazor no longer has a HeaderProfile class, adding a local version.

---
+++ src/Web/Models/HeaderProfile.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Components;
+
+namespace RivetBlazor.Classes
+{
+    // Preserved from an old release of RivetBlazor
+    public class HeaderProfile : HeaderDropdown
+    {
+        public HeaderProfile(string username, string loginUri, string logoutUri, IEnumerable<HeaderLink> links, string title = "", bool isLoggedIn = false, RenderFragment decoration = null) : base(username, links, title, "", decoration)
+        {
+            LoginUri = loginUri;
+            LogoutUri = logoutUri;
+            IsLoggedIn = isLoggedIn;
+        }
+
+        public bool IsLoggedIn { get; set; }
+        public string LoginUri { get; set; }
+        public string LogoutUri { get; set; }
+        public string Username { get; set; }
+        public string Title { get; set; }
+
+        public event Action OnProfileChanged;
+
+        public HeaderLink ConvertToLink()
+        {
+            var url = Links != null && Links.Any() ? Links.First().Uri : string.Empty;
+            return new HeaderLink(url, Username, Title, Decorations.ToArray());
+        }
+
+        public HeaderDropdown ConvertToDropdown() =>
+            new(Username, Links, Title, string.Empty, Decorations.ToArray());
+
+        public void ProfileChanged() => OnProfileChanged.Invoke();
+    }
+}
